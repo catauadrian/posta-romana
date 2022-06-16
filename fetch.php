e0f183fb3de5c6bbebe7e6c4d4c2f8076a57f39e
Catau Adrian <catauadrian@gmail.com>
2022-06-16 12:28:22 +0300

    Update fetch.php
    Improved a bit the sql query

--- fetch.php
+++ fetch.php
@@ -4,9 +4,10 @@ if (!isset($_POST['test'])) {
     exit; // disable direct access
 }
 
-
-require_once($_SERVER['DOCUMENT_ROOT'] . $folder . '/wp-load.php');
+include $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
+// require_once($_SERVER['DOCUMENT_ROOT'] . $folder . '/wp-load.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . $folder . '/wp-config.php');
+
 
 
 global $wpdb;
@@ -15,42 +16,21 @@ $query = '';
 $output = array();
 
 $anluna1 = $_POST['anluna1'];
-$query.= " select p.ID as order_id,
+$query.= " select p.ID as order_id,p.post_status,
   p.post_date as doc_date,
-  max( CASE WHEN pm.meta_key = '_billing_email' and p.ID = pm.post_id THEN pm.meta_value END ) as billing_email,
   max( CASE WHEN pm.meta_key = '_billing_first_name' and p.ID = pm.post_id THEN pm.meta_value END ) as billing_first_name,
   max( CASE WHEN pm.meta_key = '_billing_last_name' and p.ID = pm.post_id THEN pm.meta_value END ) as billing_last_name,
-  max( CASE WHEN pm.meta_key = '_billing_address_1' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_address_1,
-  max( CASE WHEN pm.meta_key = '_billing_address_2' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_address_2,
   max( CASE WHEN pm.meta_key = '_billing_city' and p.ID = pm.post_id THEN pm.meta_value END ) as billing_city,
-  max( CASE WHEN pm.meta_key = '_billing_state' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_state,
-  max( CASE WHEN pm.meta_key = '_billing_postcode' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_postcode,
-  max( CASE WHEN pm.meta_key = '_shipping_first_name' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_first_name,
-  max( CASE WHEN pm.meta_key = '_shipping_last_name' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_last_name,
-  max( CASE WHEN pm.meta_key = '_shipping_address_1' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_address_1,
-  max( CASE WHEN pm.meta_key = '_shipping_address_2' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_address_2,
-  max( CASE WHEN pm.meta_key = '_shipping_city' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_city,
-  max( CASE WHEN pm.meta_key = '_shipping_state' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_state,
-  max( CASE WHEN pm.meta_key = '_shipping_postcode' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_postcode,
   max( CASE WHEN pm.meta_key = '_order_total' and p.ID = pm.post_id THEN pm.meta_value END ) as order_total,
-  max( CASE WHEN pm.meta_key = '_order_tax' and p.ID = pm.post_id THEN pm.meta_value END ) as order_tax,
-  max( CASE WHEN pm.meta_key = '_paid_date' and p.ID = pm.post_id THEN pm.meta_value END ) as paid_date,
-  EXTRACT( YEAR_MONTH FROM p.post_date ) as anluna,
-  ( select group_concat( order_item_name separator '|' ) from wp_woocommerce_order_items where order_id = p.ID ) as order_items
-from
-  wp_posts p
+  EXTRACT( YEAR_MONTH FROM p.post_date ) as anluna
+  from wp_posts p
   join wp_postmeta pm on p.ID = pm.post_id
   join wp_woocommerce_order_items oi on p.ID = oi.order_id
-group by
-  p.ID
-HAVING
-  anluna = '".$_POST["anluna1"]."'
+group by p.ID HAVING
+  anluna = '".$_POST["anluna1"]."' and p.post_status not like 'trash'
 order by
    p.id desc ";
 
-// modify query like here // HAVING anluna = '".$_POST["anluna1"]."' and order_items like '%poştă%'
-// this is for nexus db -- select * from cli_jur_view where anluna = '".$_POST["anluna1"]."'
-// order by id desc ";
 
 
 $results = $wpdb->get_results($query);
